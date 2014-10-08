f971e4b944337ba4108339927d2d3d1250d33b0c
Rafael D. de Souza <rafaelderradi@gmail.com>
2014-10-08 11:02:23 -0300

    Adding two simple scripts to check the results.

---
+++ plot_dens.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+if len(sys.argv)!=2:
+	print 'Please, provide a file containing the densities.'
+	exit()
+
+fdens = sys.argv[1]
+
+data = np.loadtxt(fdens)
+x  = data[:,0]
+y  = data[:,1]
+d1 = data[:,2]
+d2 = data[:,3]
+df = data[:,4]
+
+fig1 = plt.figure( figsize=(8, 6) )
+gr1 = fig1.add_subplot(1, 1, 1, projection='3d')
+gr1.set_xlabel("x [fm]")
+gr1.set_ylabel('y [fm]')
+gr1.set_zlabel('density')
+gr1.plot_wireframe(x, y, d1, rstride=10, cstride=10, color='red', label='SPH density dist.')
+gr1.plot_wireframe(x, y, d2, rstride=1, cstride=1, color='blue', linestyle='dashed', label='Orig. density dist.')
+leg = gr1.legend()
+
+fig2 = plt.figure( figsize=(8, 6) )
+gr2 = fig2.add_subplot(1, 1, 1, projection='3d')
+gr2.set_xlabel("x [fm]")
+gr2.set_ylabel('y [fm]')
+gr2.set_zlabel('rel. diff.')
+gr2.plot_wireframe(x, y, df, rstride=1, cstride=1, color='black')
+
+plt.show()
---
+++ plot_dens_root.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys
+import os
+import ROOT
+
+from ROOT import gROOT, TCanvas, TGraph2D, TLegend
+
+gROOT.Reset()
+
+if len(sys.argv)!=2:
+	print 'Please, provide a file containing the densities.'
+	exit()
+
+fdens = sys.argv[1]
+
+c1 = TCanvas('c1')
+
+gr1 = TGraph2D(fdens, '%lg %lg %lg')
+gr1.SetName('SPHdens')
+gr1.SetLineColor(2)
+gr1.GetXaxis().SetTitle('x [fm]')
+gr1.GetYaxis().SetTitle('y [fm]')
+gr1.Draw('surf')
+
+gr2 = TGraph2D('ploting.dat', '%lg %lg %*s %lg')
+gr2.SetName('Oridens')
+gr2.SetLineColor(4)
+gr2.Draw('surfsame')
+
+c2 = TCanvas('c2')
+gr3 = TGraph2D('ploting.dat', '%lg %lg %*s %*s %lg')
+gr3.SetName('Densdiff')
+gr3.SetLineColor(1)
+gr3.GetXaxis().SetTitle('x [fm]')
+gr3.GetYaxis().SetTitle('y [fm]')
+gr3.Draw('surf')
+
+# wait for input to keep the GUI (which lives on a ROOT event dispatcher) alive
+if __name__ == '__main__':
+    rep = ''
+    while not rep in [ 'q', 'Q' ]:
+        rep = raw_input( 'enter "q" to quit: ' )
+        if 1 < len(rep):
+            rep = rep[0]
