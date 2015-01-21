1bffd0b361044a2970db505cfda3a114212f3709
José Hugo Elsas <jhelsas@gmail.com>
2015-01-21 16:34:19 -0200

    preparando arquivo EoS atualizada

---
+++ src/work-directory/WBeos.c
@@ -0,0 +1,26 @@
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_spline.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_roots.h>
+
+int WB_pressure(){
+}
+
+int WB_energyDen(){
+}
+
+int WB_traceDefect(){
+}
+
+int WB_entropyDen(){
+}
+
+int WB_soundSpeed(){
+}
+
+int main(){
+  int i;
+  double T;
+  
+  return 0;
+}
